7331e768b1937d9b752906ae036683b0a7e43bb0
Fred120-code <fredayemtsa@gmail.com>
2026-02-27 03:11:44 +0100

    add tabs section

--- app/Hero.jsx
+++ app/Hero.jsx
@@ -9,6 +9,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/effect-cards";
 
+//Hero
 const HeroSlides = [
   {
     bgImage: "/Hero-1.webp",
@@ -67,6 +68,7 @@ const HeroSlides = [
   },
 ];
 
+//Marquee Texts
 const marqueeItems = [
   "Timeless Architecture",
   "Bespoke Interiors",
@@ -79,6 +81,31 @@ const marqueeItems = [
   "Creative Precision",
   "Vision-Driven Planning",
 ];
+
+//About Tabs
+const tabs = [
+  { label: "History", key: "his" },
+  { label: "Mission", key: "mis" },
+  { label: "Vision", key: "vis" },
+];
+
+const tabContent = {
+  his: [
+    "At Shree Harl Associates, we design thoughtfully curated spaces that combine functionality and aesthetic excellence. Every project is shaped around our clients’ identity and long-term vision.",
+    " Our multidisciplinary team collaborates closely with you to transform ideas into refined architectural and interior experiences.",
+    "We create architectural concepts that balance innovation with practicality. Each structure is carefully planned to enhance comfort, efficiency, and visual harmony.",
+  ],
+  mis: [
+    "Every project begins with a bold idea. We translate complex requirements into clear, creative design solutions that reflect excellence and originality.",
+    " Through collaboration, precision, and innovation, we deliver spaces that inspire and add lasting value.",
+    "We approach urban and residential projects with a strategic mindset, ensuring optimal space utilization and sustainable design principles.",
+  ],
+  vis: [
+    "Our interior solutions are crafted to deliver both sophistication and functionality. We focus on spatial flow, materials, and lighting to create impactful environments.",
+    " By understanding your lifestyle and aspirations, we design interiors that feel personal, refined, and purpose-driven.",
+    " From concept development to execution, we ensure every detail aligns with your objectives and elevates the overall design outcome.",
+  ],
+};
 
 const Hero = () => {
   const [activeTab, setActiveTab] = useState("home");
@@ -171,6 +198,99 @@ const Hero = () => {
           ))}
         </div>
       </div>
+
+      {/* About */}
+      <section className="px-[8%] lg:px-[12%] py-12">
+        {/* Top Section */}
+        <div className=" flex flex-col lg:flex-row justify-between items-start gap-12">
+          <div className="lg:w-2/3">
+            <h2
+              className="text-[4rem] leading-[4rem] lg:text-[6rem] lg:leading-[6rem]
+              tracking-[0.2rem] font-[bricolage] font-bold"
+            >
+              Interior & Architecture Desing Solutions
+            </h2>
+          </div>
+          <div className="w-1/2">
+            <h3
+              className=" uppercase tracking-wider font-semibold border-b 
+            pb-2 mb-6 text-sm w-fit"
+            >
+              About Us
+            </h3>
+            <p className="text-base mb-4 text-gray-700">
+              At shree harl associates, we're comitted to transforming spaces
+              through creative vision, elegant desing, and solutions that
+              reflect your lifestyle.
+            </p>
+            <a
+              href="/about"
+              className=" inline-flex items-center
+            text-black font-semibold hover:underline text-lg"
+            >
+              Company Info <i className="bi bi-arrow-up-right ml-2 text-xs"></i>
+            </a>
+          </div>
+        </div>
+
+        {/* Main Section */}
+        <div className="mt-12 flex flex-col lg:flex-row gap-10">
+          <div className="lg:w-1/2 w-full">
+            <Swiper
+              modules={[Navigation, EffectCards, Autoplay]}
+              loop={true}
+              effect="cards"
+              grabCursor={true}
+              autoplay={{ delay: 1500 }}
+              navigation={{
+                nextEl: ".swiper-about-next",
+                prevEl: ".swiper-about-prev",
+              }}
+              className=" rounded"
+              style={{
+                padding: "30px",
+              }}
+            >
+              {[
+                "interior-image-01.jpg",
+                "interior-image-02.jpg",
+                "interior-image-03.jpg",
+                "interior-image-05.jpg",
+                "interior-image-06.jpg",
+              ].map((src, index) => (
+                <SwiperSlide key={index}>
+                  <img
+                    src={src}
+                    alt={`slide ${index + 1}`}
+                    className="w-full h-[550px] object-cover rounded"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
+          {/* Tabs & Content */}
+          <div className="lg:w-1/2 w-full px-0 lg:px-10 pt-10">
+            {/* Tabs */}
+            <div className=" flex gap-6 border-b mb-6">
+              {tabs.map((tab) => (
+                <button
+                  key={tab.key}
+                  className={`relative pb-2 text-lg font-medium transition-colors cursor-pointer
+                        ${
+                          activeTab === tab.key
+                            ? " text-black after:h-1 after:absolute after:bottom-0 after:left-0 after:w-full after:bg-black"
+                            : "text-gray-400 hover:text-black"
+                        }`}
+                  onClick={()=> setActiveTab(tab.key)}
+                >
+                  {tab.label}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };

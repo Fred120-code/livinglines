dd8bb85595a7349c884e9bc4e404ae57854e1d8d
Fred120-code <fredayemtsa@gmail.com>
2026-03-10 11:40:37 +0100

    feat: add service page

--- app/Services/page.jsx
+++ app/Services/page.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import Marquee2 from "../Component/Marquee2/Marquee2";
+import Service from "../Component/Service/Service";
+import Service2 from "../Component/Service2/Service2";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <>
+      {" "}
+      <div
+        className="relative md:h-[68vh] h-[280px] overflow-hidden flex items-center
+         justify-center text-center"
+      >
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-0 left-0 w-full object-cover z-0"
+        >
+          <source src="/page-title.mp4" type="video/mp4" />
+        </video>
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-black/70
+            z-10"
+        />
+        <h1 className="text-white text-[3rem] md:text-[5rem] font-bricolage z-20">
+          Services
+        </h1>
+      </div>
+      {/* Services */}
+      <Service />
+      {/* Marquee 2 */}
+      <Marquee2 />
+      {/* Services 2 */}
+      <Service2 />
+    </>
+  );
+};
 
-export default Page
+export default Page;

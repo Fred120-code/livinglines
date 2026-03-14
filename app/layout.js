c88b797ba1907e38080ad2687f10ad35ecc3f905
Fred120-code <fredayemtsa@gmail.com>
2026-03-14 01:06:29 +0100

    feat: configuration de GA4

--- app/layout.js
+++ app/layout.js
@@ -33,6 +33,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LFHBE5WMRY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LFHBE5WMRY');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${jost.variable} ${bricolage.variable} `}
       >

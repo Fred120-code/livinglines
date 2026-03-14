12fe7e4be8c3929a511e27971e0b4c8a6e65fe6a
Fred120-code <fredayemtsa@gmail.com>
2026-03-14 01:25:34 +0100

    feat: configuration de GA4

--- app/layout.js
+++ app/layout.js
@@ -46,23 +46,26 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-LFHBE5WMRY');
           `}
         </Script>
-        <script>
-          (function(w,d,s,l,i)
-            {w[l]=w[l]||[];
-               w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-               var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N4WMNS4G');
-        </script>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N4WMNS4G');
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${jost.variable} ${bricolage.variable} `}
       >
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4WMNS4G"
-            height="0" width="0" style="display:none;visibility:hidden">
-          </iframe>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N4WMNS4G"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
         </noscript>
         <Nav />
         {children}

b2b50618d0b00e3279750b538005b018dfc7edde
Fred120-code <fredayemtsa@gmail.com>
2026-03-14 01:19:17 +0100

    feat: configuration de GA4

--- app/layout.js
+++ app/layout.js
@@ -46,6 +46,15 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-LFHBE5WMRY');
           `}
         </Script>
+        <script>
+          (function(w,d,s,l,i)
+            {w[l]=w[l]||[];
+               w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+               var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N4WMNS4G');
+        </script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${jost.variable} ${bricolage.variable} `}
